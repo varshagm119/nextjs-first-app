9cbe162c6d1bb49ebd003fe8e0c3fa7653b6da4c
varshagm119 <varshagm96@gmail.com>
2023-11-22 18:26:26 +0530

    first commit

---
+++ src/app/aboutus/page.js
@@ -0,0 +1,5 @@
+function AboutUSPage () {
+    return <h1>About us</h1>
+ }
+
+ export default AboutUSPage;
---
+++ src/app/news/page.js
@@ -0,0 +1,5 @@
+ function NewsPage () {
+    return <h1>Hi</h1>
+ }
+
+ export default NewsPage;
