82275c507f63217ad24c2ba133b18257ef4d10dd
varshagm119 <varshagm96@gmail.com>
2023-11-22 23:20:03 +0530

    4th

--- src/app/aboutus/[id]/page.js
+++ src/app/aboutus/[id]/page.js
@@ -1,16 +1,18 @@
-import { useRouter } from "next/router";
+"use client";
+import { useRouter } from "next/navigation";
 
-const AboutUsDetail = () => {
-  const router = useRouter();
-  const { id } = router.query;
+const AboutUsDetail = ({params}) => {
+  // const router = useRouter();
+  // const { id } = router.query;
 
   const details = [
     { id: "1", name: "Yash", role: "Senior Developer" },
     { id: "2", name: "Vaibhav", role: "Backend Developer" },
     { id: "3", name: "Suresh", role: "Frontend Developer" },
   ];
+ 
 
-  const detail = details.find((item) => item.id === id);
+  const detail = details.find((item) => item.id === params);
   if (!detail) {
     return <div>Developer doesnt exist</div>;
   }
